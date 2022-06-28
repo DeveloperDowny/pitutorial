69f167e5d80b070f41677e649f658113f084eb44
SPITVedant <101183928+SPITVedant@users.noreply.github.com>
2022-06-29 00:58:31 +0530

    before refactoring

--- src/components/Home/MHome.jsx
+++ src/components/Home/MHome.jsx
@@ -1,74 +1,92 @@
 import { edited_aim_bg, how_bg } from "../../assets";
 import { Navbar2 as Navbar } from "..";
-// import { Footer, Heading1, Navbar2 as Navbar } from "";
+
 import styles from "../../../styles/MHome.module.css";
 import gsap from "gsap";
 import { useRef, useEffect } from "react";
 
+const delayTime = 6;
+const dur = 2;
+
 const Card = ({ title, subTitle, desc, imgUrl, reversed, animRef }) => {
-  const delayTime = 1;
-  const dur = 2;
-  let shouldPlayCount = 1;
-  let anim;
-  function repeatAnim() {
-    gsap
-      .from(animRef, {
-        marginLeft: `-100%`,
-        duration: dur,
+  // // const delayTime = 1;
+  // // const dur = 2;
+  // const dur2 = 0.5; // + dur;
+  // let shouldPlayCount = 1;
+  // let anim;
+  // function repeatAnim() {
+  //   console.log("in r");
+  //   console.log(shouldPlayCount);
 
-        onComplete: repeatAnim,
-        // delay: 5,
-      })
-      .delay(delayTime);
-  }
+  //   gsap
+  //     .fromTo(
+  //       animRef.current,
+  //       // marginTop: `-100%`,
+  //       { opacity: 0 },
+  //       {
+  //         opacity: 1,
+  //         duration: dur2,
 
-  useEffect(() => {
-    // console.log("in use effect");
+  //         onComplete: repeatAnim,
+  //       }
+  //     )
+  //     .delay(dur + delayTime - dur2);
+  //   // .delay(delayTime);
+  // }
 
-    shouldPlayCount += 1;
-    if (shouldPlayCount == 2) {
-      anim = gsap
-        .from(animRef, {
-          marginTop: `-100%`,
-          duration: dur,
-          onComplete: repeatAnim, //temp
-          // onComplete: () => {
-          //   counter++;
-          //   console.log(counter);
-          // },
-          // delay: 5,
-        })
-        .delay(delayTime)
-        .pause();
-      // .pause();
-      // .delay(delayTime);
-    }
+  // useEffect(() => {
+  //   shouldPlayCount += 1;
+  //   if (animRef != null && shouldPlayCount == 3) {
+  //     console.log("here");
+  //     gsap.fromTo(
+  //       animRef.current,
+  //       { opacity: 0 },
+  //       {
+  //         opacity: 1,
+  //         duration: dur2,
 
-    // anim.repeat(5);
-  }, []);
+  //         onComplete: repeatAnim,
+  //       }
+  //     );
+  //     // .delay(delayTime);
+  //   }
+  //   console.log("here");
+  //   // shouldPlayCount == 2;
+  //   // if (shouldPlayCount == 2) {
+  //   //   anim = gsap
+  //   //     .to(animRef.current, {
+  //   //       opacity: 0,
+  //   //       scale: 2,
+  //   //       // y: "100px",
+  //   //       // marginTop: `-100%`,
+  //   //       // top: "0",
+  //   //       duration: dur,
+  //   //       onComplete: repeatAnim,
+  //   //     })
+  //   //     .delay(delayTime);
+  //   // }
+  // }, []);
   return (
     <div
       style={{
         display: "flex",
-        // height: "100%",
-        width: "100vw", //absolute value
-        // justifyContent: "center",
+
+        width: "100vw",
       }}
     >
       <div
         ref={animRef}
+        // ref={animRef}
+        // id="thisOne"
         className="dropShadow"
         style={{
           color: "white",
           top: "33vh",
-          // left: "10%",
-          // right: "10%",
-          // margin: "10%",
+          opacity: 0,
+
           margin: 0,
           padding: "0 10%",
-          // height: "100vh",
-          // objectFit: "cover",
-          // width: "100%",
+
           position: "absolute",
           zIndex: "-24",
           display: "flex",
@@ -97,7 +115,7 @@ const Card = ({ title, subTitle, desc, imgUrl, reversed, animRef }) => {
           style={{
             color: "white",
             textAlign: reversed ? "right" : "left",
-            // width: "30%",
+
             lineHeight: "2.2rem",
             padding: 0,
             margin: 0,
@@ -121,16 +139,11 @@ const Card = ({ title, subTitle, desc, imgUrl, reversed, animRef }) => {
         </p>
       </div>
       <img
-        // width={"100%"}
-
         style={{
-          // display: "flex",
-          // height: "100vh",
           objectFit: "cover",
           height: "100vh",
           width: "100%",
-          // height: "100%",
-          // width: "100%",
+
           position: "absolute",
           zIndex: "-25",
         }}
@@ -138,109 +151,36 @@ const Card = ({ title, subTitle, desc, imgUrl, reversed, animRef }) => {
         alt=""
       />
     </div>
-    // <div
-    //   style={{
-    //     // zIndex: "-25",
-    //     display: "flex",
-    //     height: "100vh",
-    //     width: "100%",
-    //     margin: "0",
-    //     padding: "0",
-    //     // justifyContent: "flex-end",
-    //   }}
-    // >
-    //   <div
-    //     className="dropShadow"
-    //     style={{
-    //       color: "white",
-    //       // top: "33vh",
-    //       // left: "10%",
-    //       // right: "10%",
-    //       // margin: "10%",
-    //       margin: 0,
-    //       padding: "0 10%",
-    //       // height: "100vh",
-    //       // objectFit: "cover",
-    //       // width: "100%",
-    //       position: "absolute",
-    //       zIndex: "-24",
-    //       display: "flex",
-    //       width: "100%",
-    //       flexDirection: "column",
-    //       alignItems: reversed ? "flex-end" : "flex-start",
-    //       justifyContent: reversed ? "flex-end" : "flex-start",
-    //     }}
-    //   >
-    //     <h1
-    //       className="h1Text"
-    //       style={{
-    //         color: "white",
-    //         textAlign: reversed ? "right" : "left",
-    //         padding: 0,
-    //         margin: 0,
-    //         paddingTop: "97px",
-    //         fontFamily: "Open sans",
-    //         fontWeight: 600,
-    //       }}
-    //     >
-    //       {title}
-    //     </h1>
-    //     <p
-    //       className={styles.pText}
-    //       style={{
-    //         color: "white",
-    //         textAlign: reversed ? "right" : "left",
-    //         // width: "30%",
-    //         lineHeight: "2.2rem",
-    //         padding: 0,
-    //         margin: 0,
-    //       }}
-    //     >
-    //       {subTitle}
-    //     </p>
-    //     <p
-    //       className="pTextSmall"
-    //       style={{
-    //         color: "white",
-    //         textAlign: reversed ? "right" : "left",
-    //         width: "30%",
-    //         lineHeight: "2.2rem",
-    //         padding: 0,
-    //         margin: 0,
-    //         fontWeight: "normal",
-    //       }}
-    //     >
-    //       {desc}
-    //     </p>
-    //   </div>
-    //   <img
-    //     style={{
-    //       height: "100vh",
-    //       objectFit: "cover",
-    //       width: "100%",
-    //       position: "absolute",
-    //       zIndex: "-25",
-    //     }}
-    //     src={imgUrl.src}
-    //     alt=""
-    //   />
-    // </div>
   );
 };
 
 const MHome = () => {
   const titleRef = useRef(null);
+  const animRef1 = useRef(null);
+  const animRef2 = useRef(null);
+  const animRef3 = useRef(null);
   let marginLeftVal = 100;
   let marginLeftConst = marginLeftVal;
-  const delayTime = 1;
-  const dur = 2;
+
+  // const dur = 0.5;
   let counter = 1;
   let anim;
   let shouldPlayCount = 0;
   let numOfImages = 1;
   function repeatAnim() {
+    gsap.fromTo(
+      [animRef1.current, animRef2.current, animRef3.current],
+      // marginTop: `-100%`,
+      { opacity: 0 },
+      {
+        opacity: 1,
+        duration: "0.5",
+
+        // onComplete: repeatAnim,
+      }
+    );
+
     if (counter > numOfImages) {
-      // document.querySelector("#toAnimateHome").style["flex-direction"] = "row-reverse";
       if (document.querySelector("#toAnimateHome") == null) {
         return;
       }
@@ -248,9 +188,6 @@ const MHome = () => {
 
       marginLeftVal = 0;
       counter = 0;
-      // return;
-      // ("this is kind of working. You would need to have last image same as the first. Then programmatically reset every marginLeftVal etc to run smoothly")
-      // return;
     }
     marginLeftVal += marginLeftConst;
     console.log(marginLeftVal);
@@ -259,37 +196,66 @@ const MHome = () => {
       .to("#toAnimateHome", {
         marginLeft: `-${marginLeftVal}%`,
         duration: dur,
+        onStart: () => {
+          gsap.fromTo(
+            [animRef1.current, animRef2.current, animRef3.current],
+            // marginTop: `-100%`,
+            { opacity: 1 },
+            {
+              opacity: 0,
+              duration: "0",
+              stagger: "0.1",
 
+              // onComplete: repeatAnim,
+            }
+          );
+        },
         onComplete: repeatAnim,
-        // delay: 5,
       })
       .delay(delayTime);
   }
   useEffect(() => {
-    // console.log("in use effect");
     shouldPlayCount += 1;
     if (shouldPlayCount == 2) {
+      gsap.fromTo(
+        [animRef1.current, animRef2.current, animRef3.current],
+        // marginTop: `-100%`,
+        { opacity: 0 },
+        {
+          opacity: 1,
+          duration: "0.5",
+          stagger: "0.1",
+
+          // onComplete: repeatAnim,
+        }
+      );
+
       anim = gsap
         .to("#toAnimateHome", {
           marginLeft: `-${marginLeftVal}%`,
           duration: dur,
-          onComplete: repeatAnim, //temp
-          // onComplete: () => {
-          //   counter++;
-          //   console.log(counter);
-          // },
-          // delay: 5,
+          onComplete: repeatAnim,
+          onStart: () => {
+            gsap.fromTo(
+              [animRef1.current, animRef2.current, animRef3.current],
+              // marginTop: `-100%`,
+              { opacity: 1 },
+              {
+                opacity: 0,
+                duration: "0",
+
+                // onComplete: repeatAnim,
+              }
+            );
+          },
         })
         .delay(delayTime);
       // .pause();
-      // .delay(delayTime);
     }
-
-    // anim.repeat(5);
   }, []);
   return (
     <div>
-      <Navbar whichActive={"results"} />
+      <Navbar whichActive={"none"} />
       <div
         ref={titleRef}
         id="toAnimateHome"
@@ -298,24 +264,10 @@ const MHome = () => {
           flexDirection: "row",
           width: "max-content",
           height: "100%",
-          // height: "100%",
         }}
-        // style={{
-        //   // position: "absolute",
-        //   height: "100vh",
-        //   // zIndex: -12,
-        //   // marginLeft: "-3rem", // animate this property
-        //   display: "flex",
-        //   flexDirection: "row",
-        //   width: "max-content",
-        //   // overflowX: "scroll",
-        //   // overflowY: "hidden",
-        //   // msOverflowStyle: "none",
-        //   // scrollbarWidth: "none",
-        // }}
       >
         <Card
-          // animRef={useRef(null)}
+          animRef={animRef1}
           title={"OUR MISSION"}
           subTitle={"Is To Unlock Hidden Potential"}
           desc={
@@ -324,6 +276,7 @@ const MHome = () => {
           imgUrl={edited_aim_bg}
         />
         <Card
+          animRef={animRef2}
           title={"BETTER EDUCATION "}
           subTitle={"For Better Future "}
           desc={
@@ -334,6 +287,7 @@ const MHome = () => {
         />
 
         <Card
+          animRef={animRef3}
           title={"OUR MISSION"}
           subTitle={"Is To Unlock Hidden Potential"}
           desc={
